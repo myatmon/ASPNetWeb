db47aaf46e97f7c612e6a7d746364319e0f23347
monshein <monshein@idlinksystems.com>
2020-11-17 17:39:59 +0800

    adding data connection by using data factory pattern

---
+++ ASPNet/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+
+namespace ASPNet
+{
+    public abstract class Database
+    {
+        public string connectionString;
+
+        #region Abstract Functions
+
+        public abstract IDbConnection CreateConnection();
+        public abstract IDbCommand CreateCommand();
+        public abstract IDbConnection CreateOpenConnection();
+        public abstract IDbCommand CreateCommand(string commandText, IDbConnection connection);
+        public abstract IDbCommand CreateStoreProcCommand(string procName, IDbConnection connection);
+        public abstract IDataParameter CreateParameter(string parameterName, object dataValue);
+
+        #endregion
+    }
+}
---
+++ ASPNet/DatabaseFactorySectionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Configuration;
+
+namespace ASPNet
+{
+    public class DatabaseFactorySectionHandler : ConfigurationSection
+    {
+        [ConfigurationProperty("Name")]
+        public string Name
+        {
+            get { return (string)base["Name"]; }
+        }
+
+        [ConfigurationProperty("ConnectionStringName")]
+        public string connectionStringName
+        {
+            get { return (string)base["ConnectionStringName"]; }
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                try
+                {
+                    return ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception("Connection string " + connectionStringName + " was not found in web.config. " + ex.Message)
+;                }
+            }
+        }
+    }
+}
