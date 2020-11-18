12e0421ebd9a40294d1bb58126532f786b697118
monshein <monshein@idlinksystems.com>
2020-11-18 15:52:01 +0800

    adding utility

---
+++ ASPNet/Utility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ASPNet
+{
+    interface Utility
+    {
+        public bool checkExtension(File file);
+    }
+}
