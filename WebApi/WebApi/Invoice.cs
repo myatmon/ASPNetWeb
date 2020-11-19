560fe37c5f926afd8630761e44fe56b2b0ef5c6c
monshein <monshein@idlinksystems.com>
2020-11-19 09:32:53 +0800

    adding model for web api

---
+++ WebApi/WebApi/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi
+{
+    public class Invoice
+    {
+        public string invoiceId { get; set; }
+        public decimal amount { get; set; }
+        public DateTime datetTime { get; set; }
+    }
+}
---
+++ WebApi/WebApi/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Transaction
+    {
+        public string transactionId { get; set; }
+        public DateTime transactionDate { get; set; }
+        public Invoice invoice { get; set; }
+        public string status { get; set; }        
+    }
+}
